dbe822c199d3e926f3ff52f8b538d84212d276c2
Izann_Brum <61673451+Izann-Brum@users.noreply.github.com>
2022-06-17 14:11:41 -0300

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 <body class="d-flex flex-column min-vh-100">
     <div class="wrapper flex-grow-1"></div>
-    <footer class="page-footer" style="background-color: #021A37; left: 0; bottom: 0; width: 100%; height: 75px; text-align: center;">
+    <footer class="page-footer" style="background-color: #021A37; left: 0; bottom: 0; width: 100%; height: 50px; text-align: center;">
         <div class="footer-copyright text-center py-3">
             <a style="color:white;"> © 2022: Banco de dados, Desenvolvido por Izann R. G. Brum e João P. S. Cunha.</a>
         </div>
