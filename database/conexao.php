bc9f4d9f4152a1664e3662ecf81b5a5d0d424790
Izann <izannrogerio@gmail.com>
2022-06-17 18:18:01 -0300

    confirmar em 'main'

--- database/conexao.php
+++ database/conexao.php
@@ -28,6 +28,7 @@ class Connection {
         $pdo = mysqli_connect($servername, $username, $password, $database);
         // Check connection
         if (!$pdo) {
+            echo('Connection failed');
             die("Connection failed: " . mysqli_connect_error());
         }else{
             // echo " Connected successfully ";
