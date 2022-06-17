08be6fc217457349edefd914261fd0d044652554
Izann <izannrogerio@gmail.com>
2022-06-17 19:18:25 -0300

    n

--- database/database.ini.php
+++ database/database.ini.php
@@ -1,7 +1,7 @@
 <?php
-
+echo(' data ');
 require 'conexao.php';
-
+echo(' database ');
 use ConexaoPHPPostgres\Connection as Connection;
 echo(' database.ini ');
 $pdo = Connection::get()->connect();
