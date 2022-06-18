3e7658706deace12a7614777df8445071bbbc08a
Izann <izannrogerio@gmail.com>
2022-06-18 16:52:46 -0300

    n

--- database/conexao.php
+++ database/conexao.php
@@ -8,27 +8,16 @@ class Connection {
     public function connect(){
         echo(' a.0 ');
 
-        // // Le os parametros do banco do dados -> database.ini
-        // $params = parse_ini_file('database.ini');
-        // if ($params === false) {
-        //     throw new \Exception("Error reading database configuration file");   
-        // }
-        // // Conecta ao postgres
-        //  $conStr = sprintf("pgsql:host=%s;dbname=%s;user=%s;password=%s", 
-        //         $params['host'], 
-        //         $params['database'], 
-        //         $params['user'], 
-        //         $params['password']);
     
         $servername = "us-cdbr-east-05.cleardb.net";
         $database = "heroku";
         $username = "bc721df62a39bf";
         $password = "bd4ca0ae";
-        echo(' a.0 ');
+        echo(' b.0 ');
         // Create connection
         $pdo = mysqli_connect($servername, $username, $password) or die ("html>script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)/script>/html>");
         mysqli_select_db($dbname);
-        echo(' b.0 ');
+        echo(' c.0 ');
         // Check connection
         if (!$pdo) {
             echo('Connection failed');
