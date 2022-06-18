1f8b15f442a0a68de579120dd5fbd40e1681d537
Izann <izannrogerio@gmail.com>
2022-06-18 17:02:40 -0300

    s

--- database/conexao.php
+++ database/conexao.php
@@ -16,7 +16,7 @@ class Connection {
         echo(' b.0 ');
         // Create connection
         $pdo = mysqli_connect($servername, $username, $password) or die ("html>script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)/script>/html>");
-        mysqli_select_db($database);
+        // mysqli_select_db($database);
         echo(' c.0 ');
         // Check connection
         if (!$pdo) {
